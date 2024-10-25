d83dbaf55f8132e3cba3e36211ad593779d08c21
kylepeart21 <kylepeart12>
2024-10-24 23:39:18 -0500

    Add alternating X and O to squares on click

--- tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMCintentLoaded', function(){
     const squares = document.querySelectorAll('#game-board div');
 
+        let currentPlayer = 'X';
+
+        let gameState = Array(squares.length).fill(null);
     squares.forEach(function(square){
         square.classList.add('square');
+
+        square.addEventListener('click',function(){
+            if(!gameState[index]){
+                square.textContent = currentPlayer;
+                square.classList.add(currentPlayer);
+                gameState[index] = currentPlayer;
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        });
     });
 });
