4d60b482a7cafe90319f622306c9573852a5c7fa
kylepeart21 <kylepeart12>
2024-10-24 23:52:52 -0500

    Added win detection and winner message display

--- tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -4,16 +4,55 @@ document.addEventListener('DOMCintentLoaded', function(){
         let currentPlayer = 'X';
 
         let gameState = Array(squares.length).fill(null);
+
+        const winningCombinations = [
+            [0, 1, 2], 
+            [3, 4, 5], 
+            [6, 7, 8], 
+            [0, 3, 6], 
+            [1, 4, 7], 
+            [2, 5, 8], 
+            [0, 4, 8], 
+            [2, 4, 6]  
+        ];
+
+        let gameOver = false;
+
+        function checkForWinner(){
+            for(const combination of winningCombinations){
+                const [a,b,c] = combination;
+                if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]){
+                    return gameState[a];
+                }
+            }
+            return null;
+        }
+
+    function displayWinner(wwinner){
+        const statusDiv = document.getElementById('status');
+        statusDiv.textContent = 'COngratulations! ${winner} is the winner';
+        statusDiv.classList.add('you-won');
+    }
+    
     squares.forEach(function(square){
         square.classList.add('square');
 
-        square.addEventListener('click',function(){
-            if(!gameState[index]){
-                square.textContent = currentPlayer;
-                square.classList.add(currentPlayer);
-                gameState[index] = currentPlayer;
-                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        square.addEventListener('click', function() {
+            if (gameOver || gameState[index]) return; 
+            square.textContent = currentPlayer;
+            square.classList.add(currentPlayer); 
+            gameState[index] = currentPlayer; 
+            const winner = checkForWinner();
+            if (winner) {
+                gameOver = true;
+                displayWinner(winner); 
+            } else {
+                currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
             }
+        });
+
+        square.addEventListener('mouseover', function() {
+            square.classList.add('hover');
         });
 
         square.addEventListener('mouseout',function(){
